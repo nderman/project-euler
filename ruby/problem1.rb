a4e4b5e0499b7c3f18200c21a332a3b99220e885
Neal <neal.derman@hetzner.co.za>
2019-07-18 14:43:49 +0200

    problem 1 solved using series More mathematical solution to the problem using arithmetic series

--- ruby/problem1.rb
+++ ruby/problem1.rb
@@ -4,3 +4,8 @@ sum = 0
   sum += i if (i % 3).zero? || (i % 5).zero?
 end
 puts sum
+
+# problem 1 solved using numeric series identity sum = n/2*(n+1)
+x = 1000
+sum = 3 * ((x - 1) / 3) * (((x - 1) / 3) + 1) / 2 + 5 * ((x - 1) / 5) * (((x - 1) / 5) + 1) / 2  - 15 * ((x - 1) / 15) * (((x - 1) / 15) + 1) / 2
+puts sum
